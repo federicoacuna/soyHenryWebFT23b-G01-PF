d7ef7ca5f9875fc33a1cd964e78f28fdb6f03f86
emanuel <emasaucedo282@gmail.com>
2022-05-09 16:20:33 -0300

    feat(client/components/NavBar): Create NavBar placeholder

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,1 +1,21 @@
-// sadsadsadsadsada
+import React from 'react'
+import s from './index.module.css'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <div className={s.container}>
+      <div>LOGO</div>
+      <nav>
+        <ul>
+          <li><Link>Contacto</Link></li>
+          <li><Link>Marcas</Link></li>
+          <li><Link>Productos</Link></li>
+          <li><Link>Home</Link></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
