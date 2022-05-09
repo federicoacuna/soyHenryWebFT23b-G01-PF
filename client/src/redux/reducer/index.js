af103c6df50594bb2be5804fe7a54b2884ad91f1
Luis Nicolás Migliore Sabadini <migliorenicolas@yahoo.com.ar>
2022-05-09 16:41:35 -0300

    feat(client/src/redux/reducer): add GET_T_CATEGORY ca

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,12 +1,18 @@
+import { GET_PRODUCTS, GET_CATEGORIES } from '../constants'
 import { } from '../actions'
 
 const initialState = {
-
+  products: [],
+  categories: []
 }
 
 const reducer = (state = initialState, action) => {
   const { payload, type } = action
   switch (type) {
+    case GET_PRODUCTS:
+      return { ...state, products: payload }
+    case GET_CATEGORIES:
+      return { ...state, categories: payload }
     default:
       return state
   }
