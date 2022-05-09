e3ed2641d9eb397dff2505755c36df94adeb6c09
brayanobisto <brayan-28-@hotmail.com>
2022-05-09 12:04:49 -0500

    feat(client/index): add store and add default axios baseUrl

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import axios from 'axios'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+// Setup default base url for axios requests
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
