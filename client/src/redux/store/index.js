c91feeefe33fcf71309fe49a96f15da2946e4dc5
brayanobisto <brayan-28-@hotmail.com>
2022-05-09 11:45:25 -0500

    fix(client/store): reducer and redux devtools imports

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,8 +1,8 @@
 import { applyMiddleware, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from '@redux-devtools/extension'
 import thunk from 'redux-thunk'
 
-import reducer from '../reducer/reducer'
+import reducer from '../reducer'
 
 const store = createStore(
   reducer,
