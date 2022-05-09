759651a3a2f2fbdac4cf040b63e6e50326e71b82
Sebastián Cerutti <softsys95@gmail.com>
2022-05-09 17:23:16 -0300

    feat(client/component/SearchBar): adding search functionalities.

--- client/src/components/SearchBar/index.jsx
+++ client/src/components/SearchBar/index.jsx
@@ -1,10 +1,38 @@
-import styles from './SearchBar.module.css'
+import { useDispatch } from 'react-redux'
+import { useState } from 'react'
+import { getProducts } from '../../redux/actions/index'
+import styles from './index.module.css'
 
 export default function SearchBar () {
+  const dispatch = useDispatch()
+  const [item, setItem] = useState('')
+
+  function handleInputChange (e) {
+    e.prventDefault()
+    setItem(e.target.value)
+  }
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    if (item.lenght === 0) {
+      return (
+        alert('Please write an item to search')
+      )
+    } else {
+      dispatch(getProducts(item))
+      setItem('')
+    }
+  }
+
   // placeholder
   return (
     <div className={styles.searchBarContainer}>
-      <input type='text' placeholder='start typing to search' className={styles.searchBox} />
+      <input
+        type='text' placeholder='start typing to search' className={styles.searchBox}
+        value={item}
+        onKeyPress={e => e.key === 'Enter' && handleSubmit(e)}
+        onChange={e => handleInputChange(e)}
+      />
       <input type='button' value='Search' className={styles.searchButton} />
     </div>
   )
