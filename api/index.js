8867a83914369cc77fdffdffdeb4e20c7e4c740c
brayanobisto <brayan-28-@hotmail.com>
2022-05-10 15:47:28 -0500

    feat(api/data seeder): add default data

--- api/index.js
+++ api/index.js
@@ -33,6 +33,7 @@ const { conn } = require('./src/db.js')
 const port = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
+  require('./seeder.js')
   server.listen(port, () => {
     console.log('Backend listening at 3001') // eslint-disable-line no-console
   })
---
+++ api/seeder.js
@@ -0,0 +1,85 @@
+const { Product, Category } = require('./src/db')
+const { faker } = require('@faker-js/faker')
+
+const categories = [
+  'Portátiles',
+  'Mouses',
+  'Teclados',
+  'Monitores',
+  'Headsets',
+  'Webcams',
+  'Tablets',
+  'Smartphones',
+  'Consolas',
+  'Alfombrillas'
+]
+
+const images = [
+  // Laptops
+  ['https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/81PbOX7ZtaL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/71nz3cIcFOL._AC_SX679_.jpg'],
+  // Mouses
+  ['https://m.media-amazon.com/images/I/61MI2KPnKgL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/614OAIJD7tL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/61VwBIfNvJL._AC_SX679_.jpg'],
+  // Teclados
+  ['https://m.media-amazon.com/images/I/61hdRFcTkzL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/61M8Wj7Ds6L._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/61w0BypBzrL._AC_SX679_.jpg'],
+  // Monitores
+  ['https://m.media-amazon.com/images/I/91ubktnbNVL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/71mU5rpECpL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/613QjYRkKqL._AC_SX679_.jpg'],
+  // Headsets
+  ['https://m.media-amazon.com/images/I/61Uviy0iWRL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/51CSzjwiYyL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/71g5MRbP52L._AC_SX679_.jpg'],
+  // Webcams
+  ['https://m.media-amazon.com/images/I/51rXag1Mg9S._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/61OQP2+yqML._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/61N5gyQPyPL._AC_SX679_.jpg'],
+  // Tablets
+  ['https://m.media-amazon.com/images/I/614M3bS7zBL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/61nNYx6SdML._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/71jzm4PpqHL._AC_SX679_.jpg'],
+  // Smartphones
+  ['https://m.media-amazon.com/images/I/71jfKwFR1rL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/61iJ6RQS-eL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/716n8eAia+L._AC_SX679_.jpg'],
+  // Consolas
+  ['https://m.media-amazon.com/images/I/61k9NXMP82S._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/61y0N1Si-DL._SX522_.jpg',
+    'https://m.media-amazon.com/images/I/6174zFQ0Z3L._AC_SX679_.jpg'],
+  // Alfombrillas
+  ['https://m.media-amazon.com/images/I/71JNnisTACL._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/817JBnouy6S._AC_SX679_.jpg',
+    'https://m.media-amazon.com/images/I/71OyF+xalUL._AC_SX679_.jpg']
+]
+
+const saveProducts = async () => {
+  for (let i = 0; i < categories.length; i++) {
+    try {
+      const category = await Category.create({ name: categories[i] })
+
+      for (let j = 0; j < images[i].length; j++) {
+        const product = await Product.create({
+          name: faker.lorem.sentence(),
+          description: faker.lorem.paragraph(),
+          brand: faker.company.companySuffix(),
+          model: faker.random.alphaNumeric(7),
+          price: faker.commerce.price(15, 1000, 2),
+          img: [images[i][j]]
+        })
+
+        await product.addCategory(category.id)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+saveProducts()
+  .then(() => console.log('Productos creados'))
+  .catch(error => console.log(error))
