9cdb0bb6dd284215440048b0b200b43b9b1c2c18
emanuel <emasaucedo282@gmail.com>
2022-05-10 14:30:52 -0300

    feat(client): add new component FilterContainer

---
+++ client/src/components/FilterContainer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './index.module.css'
+
+const FilterContainer = () => {
+  return (
+    <div className={s.container}>
+      ACA VAN LOS FILTRADOS POR PRECIO, MARCA Y CATEGORIA
+    </div>
+  )
+}
+
+export default FilterContainer
--- client/src/pages/UsersHome/index.jsx
+++ client/src/pages/UsersHome/index.jsx
@@ -1,6 +1,7 @@
 import ProductCard from '../../components/ProductCard'
 import NavBar from '../../components/NavBar'
 import SearchBar from '../../components/SearchBar'
+import FilterContainer from '../../components/FilterContainer'
 
 function UsersHome () {
   return (
@@ -8,6 +9,7 @@ function UsersHome () {
       <NavBar />
       <SearchBar />
       <ProductCard />
+      <FilterContainer />
     </div>
   )
 }
