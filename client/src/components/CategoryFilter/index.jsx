d532447b8e57d86aeb4b8975fceb22ab89131dbe
mutriaxx <marcelautria@gmail.com>
2022-05-10 12:48:36 -0500

    Feat(client): Component CategoryFilter added.

---
+++ client/src/components/CategoryFilter/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const CategoryFilter = () => {
+  // useEffect(()=>{
+
+  // },[])
+  return (
+    <div>
+      <ul>
+        <li>Categoria 1</li>
+        <li>Categoria 2</li>
+        <li>Categoria 3</li>
+        <li>Categoria 4</li>
+        <li>Categoria 5</li>
+      </ul>
+    </div>
+  )
+}
+
+export default CategoryFilter
