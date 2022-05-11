5ac13cb9f54320afe670b75fa7a51e9ab102577f
Luis Nicolás Migliore Sabadini <migliorenicolas@yahoo.com.ar>
2022-05-11 18:49:23 -0300

    feat(API/.../services/userAdresses.service): added file userAdresses.service

---
+++ api/src/services/userAdresses.service.js
@@ -0,0 +1,35 @@
+// VERIFICAR que el nombre del modelo Adresses coincida con el de la DB
+const { Adresses } = require('../db')
+
+async function getAllAdresses (useriD) {
+  return await Adresses.findByPk(useriD)
+}
+
+async function createAdress (data) {
+  // VERIFICAR que nombres de parámetros coincidan con los del modelo Adresses de la DB
+  const { postalCode, state, city, streetName, houseNumber, floorApartment, deliveryInstructions } = data
+
+  return await Adresses.findOrCreate({
+    where: {
+      postalCode,
+      state,
+      city,
+      streetName,
+      houseNumber,
+      floorApartment,
+      deliveryInstructions
+    }
+  })
+}
+
+async function removeAdress (adressId) {
+  return await Adresses.destroy({
+    where: { id: adressId }
+  })
+}
+
+module.exports = {
+  getAllAdresses,
+  createAdress,
+  removeAdress
+}
