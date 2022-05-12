6f55617e297bcc11619c5352825533d5b2ad651d
Jyankc <juanyancovich@gmail.com>
2022-05-12 16:05:45 -0300

    feat(api/src/models):new models added with their associations

--- api/src/db.js
+++ api/src/db.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize')
 const fs = require('fs')
 const path = require('path')
+const { resolveSoa } = require('dns')
 const {
   DB_USER, DB_PASSWORD, DB_HOST
 } = process.env
 
-const DB_URL = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/ecommerce`
+const DB_URL = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:3000/ecommerce`
 
 const sequelize = new Sequelize(DB_URL, {
   logging: false, // set to console.log to see the raw SQL queries
@@ -32,11 +33,65 @@ sequelize.models = Object.fromEntries(capsEntries)
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Category, Product, Role, User, Branch, CartItem, Inventory } = sequelize.models
+const { Category, Product, Role, User, UserAddress, UserPayment, Branch, CartItem, Inventory, Order, OrderItem, PaymentType, WishList, Review } = sequelize.models
 
 // Aca vendrian las relaciones
-Category.hasMany(Product, { foreingKey: 'categoryId', allowNull: false })
+Category.hasMany(Product, { foreingKey: { allowNull: false } })
 Product.belongsTo(Category)
+
+User.hasMany(UserAddress)
+UserAddress.belongsTo(User)
+
+User.hasMany(UserPayment, { foreingKey: { allowNull: false } })
+UserPayment.belongsTo(User)
+
+PaymentType.hasMany(UserPayment)
+UserPayment.belongsTo(PaymentType)
+
+Role.hasMany(User)
+User.belongsTo(Role)
+
+User.hasMany(Review)
+Review.belongsTo(User)
+
+Product.hasMany(Review)
+Review.belongsTo(Product)
+
+User.hasMany(CartItem)
+CartItem.belongsTo(User)
+
+Product.hasMany(CartItem)
+CartItem.belongsTo(Product)
+
+Product.belongsToMany(Inventory, { through: 'product_inventory' })
+Inventory.belongsToMany(Product, { through: 'product_inventory' })
+
+Branch.hasOne(Inventory)
+Inventory.belongsTo(Branch)
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+UserPayment.hasOne(Order)
+Order.belongsTo(UserPayment)
+
+UserAddress.hasOne(Order)
+Order.belongsTo(UserAddress)
+
+Branch.hasOne(Order)
+Order.belongsTo(Branch)
+
+OrderItem.hasMany(Order)
+Order.belongsTo(OrderItem)
+
+Product.hasMany(OrderItem)
+OrderItem.belongsTo(Product)
+
+User.hasOne(WishList)
+WishList.belongsTo(User)
+
+Product.hasOne(WishList)
+WishList.belongsTo(Product)
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
---
+++ api/src/models/Review.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+  sequelize.define('review', {
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        min: 1,
+        max: 5,
+        isInt: true
+      },
+      review: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      }
+    }
+  })
+}
