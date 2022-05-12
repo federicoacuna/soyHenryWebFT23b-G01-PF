429922efaa2fd27973d9d392f58be30fd31bc6dc
emanuel <emasaucedo282@gmail.com>
2022-05-12 16:13:55 -0300

    feat(client): installing chakra UI

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import axios from 'axios'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 
 // Setup default base url for axios requests
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001'
@@ -13,10 +14,13 @@ axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   // <React.StrictMode>
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>
+  <ChakraProvider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </ChakraProvider>
+
   // </React.StrictMode>
 )
