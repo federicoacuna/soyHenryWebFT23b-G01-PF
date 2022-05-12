4c21259571cf331c33b834f94dc51d3af6ac82f9
emanuel <emasaucedo282@gmail.com>
2022-05-12 14:35:28 -0300

    feat(client): Design layout from component Cart

--- client/src/pages/Cart/index.jsx
+++ client/src/pages/Cart/index.jsx
@@ -1,17 +1,22 @@
 import { useSelector } from 'react-redux'
 import CartItems from '../../components/CartItems'
 import CartDeleteAllProducts from '../../components/CartDeleteAllProducts'
+import s from './index.module.css'
 
 export const Cart = () => {
   const cartProducts = useSelector(state => state.cartProducts)
 
   return (
-    <div>
-      <h1>Carrito</h1>
-      <CartItems />
-      <CartDeleteAllProducts />
-      <p>Total: {cartProducts.reduce((acc, p) => acc + (p.quantity * p.price), 0)}</p>
+    <div className={s.containerAll}>
+      <div className={s.container}>
+        <h1>Carrito</h1>
+        <CartDeleteAllProducts />
+        <CartItems />
+        <p>Total: {cartProducts.reduce((acc, p) => acc + (p.quantity * p.price), 0)}</p>
+        <button>Continuar compra</button>
+      </div>
     </div>
+
   )
 }
 
