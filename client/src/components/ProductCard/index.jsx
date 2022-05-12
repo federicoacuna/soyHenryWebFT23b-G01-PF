59b9b0e7a4b8177d4db86b89648f48a3699e9a61
mutriaxx <marcelautria@gmail.com>
2022-05-12 15:53:47 -0500

    fix(client/src): CartItem and ProductCard components were fixed.

--- client/src/components/ProductCard/index.jsx
+++ client/src/components/ProductCard/index.jsx
@@ -2,17 +2,17 @@ import CartButton from '../CartButton'
 import styles from './index.module.css'
 
 export default function ProductCard ({ product }) {
-  const { name, price, img, category } = product
-
+  const { name, price, image, category } = product
+  console.log(product)
   return (
     <div className={styles.cardContainer}>
-      <h3 className={styles.productTitle}>{name}</h3>
-      <img src={img[0]} alt='Product' className={styles.productImage} />
+      <h3 className={styles.productTitle}>{name && name}</h3>
+      <img src={image && image[0]} alt='Product' className={styles.productImage} />
       <p className={styles.productInformation}>
-        Categoria: {category.name}
+        Categoria: {category.name && category.name}
       </p>
       <p className={styles.productInformation}>Precio: {price}</p>
-      <CartButton product={product} />
+      <CartButton product={product && product} />
     </div>
   )
 }
