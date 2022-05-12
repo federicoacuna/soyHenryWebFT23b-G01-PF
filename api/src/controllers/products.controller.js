7d50ddf0963e297dd7a2bd58244cdebf947e55f9
Federico Acuna <federicoa1982@gmail.com>
2022-05-12 18:24:28 -0300

    fix(API/products route->service)
    Integration debug for products endpoint
    bulk product requests with full search and filter capabilities
    and product request by id now working

--- api/src/controllers/products.controller.js
+++ api/src/controllers/products.controller.js
@@ -1,17 +1,20 @@
-const services = require('../services/products.service')
+const Products = require('../services/products.service')
 
 async function get (req, res, next) {
   try {
-    res.json(await services.getProducts(req.query))
+    const retrievedProducts = await Products.getProducts(req.query)
+    console.log(retrievedProducts)
+    retrievedProducts ? res.json(retrievedProducts) : res.status(404).json({ error: 'No products where found' })
   } catch (error) {
-    console.log('error trying to execute getProducts')
+    res.json(error)
   }
 }
 async function getById (req, res, next) {
   try {
-    res.json(await services.getProductDetail(req.param.id))
+    const retrievedProduct = await Products.getProductDetail(req.params.id)
+    retrievedProduct ? res.json(retrievedProduct) : res.status(404).json({ error: 'Requested product was not found' })
   } catch (error) {
-    console.log('error trying to execute getProducts')
+    res.json(error)
   }
 }
 
