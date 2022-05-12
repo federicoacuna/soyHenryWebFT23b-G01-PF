5025bf01b8c453503ac001ba12a61e76ca944c87
Jyankc <juanyancovich@gmail.com>
2022-05-12 00:09:25 -0300

    (api/src/models):fixed syntax error

--- api/src/models/CartItem.js
+++ api/src/models/CartItem.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-  sequelize.define('cart_item', {
+  sequelize.define('cartItem', {
     quantity: {
       type: DataTypes.INTEGER,
       validate: {
