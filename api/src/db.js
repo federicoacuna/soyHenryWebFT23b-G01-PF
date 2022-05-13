46fcfa1f83b47697619f679056e25edc58ac1cf2
Federico Acuna <federicoa1982@gmail.com>
2022-05-13 10:13:27 -0300

    fix(API/models utils)

--- api/src/db.js
+++ api/src/db.js
@@ -16,22 +16,17 @@ const sequelize = new Sequelize(DB_URL, {
 const basename = path.basename(__filename)
 const modelDefiners = []
 
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)))
   })
 
-// Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize))
-// Capitalizamos los nombres de los modelos ie: product => Product
 const entries = Object.entries(sequelize.models)
 const capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]])
 sequelize.models = Object.fromEntries(capsEntries)
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
 const { Category, Product, Role, User, UserAddress, UserPayment, Branch, CartItem, Inventory, Order, OrderItem, PaymentType, WishList, Review } = sequelize.models
 
 // Aca vendrian las relaciones
@@ -50,49 +45,34 @@ UserPayment.belongsTo(PaymentType)
 Role.hasMany(User)
 User.belongsTo(Role)
 
-User.hasMany(Review)
-Review.belongsTo(User)
+User.belongsToMany(Product, { through: 'review' })
+Product.belongsToMany(User, { through: 'review' })
 
-Product.hasMany(Review)
-Review.belongsTo(Product)
+User.belongsToMany(Product, { through: 'cartItem' })
+Product.belongsToMany(User, { through: 'cartItem' })
 
-User.hasMany(CartItem)
-CartItem.belongsTo(User)
-
-Product.hasMany(CartItem)
-CartItem.belongsTo(Product)
-
-Product.belongsToMany(Inventory, { through: 'product_inventory' })
-Inventory.belongsToMany(Product, { through: 'product_inventory' })
-
-Branch.hasOne(Inventory)
-Inventory.belongsTo(Branch)
+Product.belongsToMany(Branch, { through: 'inventory' })
+Branch.belongsToMany(Product, { through: 'inventory' })
 
 User.hasMany(Order)
 Order.belongsTo(User)
 
-UserPayment.hasOne(Order)
+UserPayment.hasMany(Order)
 Order.belongsTo(UserPayment)
 
-UserAddress.hasOne(Order)
+UserAddress.hasMany(Order)
 Order.belongsTo(UserAddress)
 
-Branch.hasOne(Order)
+Branch.hasMany(Order)
 Order.belongsTo(Branch)
 
-OrderItem.hasMany(Order)
-Order.belongsTo(OrderItem)
+Product.belongsToMany(Order, { through: 'orderItem' })
+Order.belongsToMany(Product, { through: 'orderItem' })
 
-Product.hasMany(OrderItem)
-OrderItem.belongsTo(Product)
-
-User.hasOne(WishList)
-WishList.belongsTo(User)
-
-Product.hasOne(WishList)
-WishList.belongsTo(Product)
+User.belongsToMany(Product, { through: 'wishlist' })
+Product.belongsToMany(User, { through: 'wishlist' })
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models,
+  conn: sequelize
 }
--- api/src/models/WhishList.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = (sequelize) => {
-  sequelize.define('wishList', {})
-}
