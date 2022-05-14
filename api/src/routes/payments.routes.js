59cbcf8a9a26ce211d9b16f228a3dc4e647d16dd
Federico Acuna <federicoa1982@gmail.com>
2022-05-14 17:51:57 -0300

    fix(API/Payments): debugged route
    Endpoint now retrieves the payment methods that belong to a user and creates a new one.
    userPayments model needs to be FIXED by removing credit card validation

--- api/src/routes/payments.routes.js
+++ api/src/routes/payments.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const router = Router()
-const { create, remove, get } = require('../controllers/payments.controller')
+const { create, remove, get } = require('../controllers/userPayments.controller')
 
 router.get('/:userId', get)
 
