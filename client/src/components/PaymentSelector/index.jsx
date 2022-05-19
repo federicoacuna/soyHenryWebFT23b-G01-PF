06e6c4f90713f0e33683dd171678a33d08c5e433
brayanobisto <brayan-28-@hotmail.com>
2022-05-19 15:21:23 -0500

    feat(client/mercadopago)

--- client/src/components/PaymentSelector/index.jsx
+++ client/src/components/PaymentSelector/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch/* , useSelector */ } from 'react-redux'
 import { setUserPayment } from '../../redux/actions'
 import PaymentCard from '../PaymentCard'
 import { Text, Flex } from '@chakra-ui/react'
 
 export default function PaymentSelector ({ children }) {
-  const userPayments = useSelector(state => state.user.userPayments)
+  // const userPayments = useSelector(state => state.user.userPayments)
   const dispatch = useDispatch()
 
   return (
     <Flex justifyContent='center' alignItems='center' h='80vh' w='100vw'>
       <Flex flexDirection='column' borderRadius={5} boxShadow='lg' bg='secondary' w='70vw' p={5}>
         <Text p={0} fontSize='2xl' fontWeight='500' mb={2}>Métodos de pago </Text>
-        {userPayments && userPayments.map((payment) => (
+        {/* {userPayments && userPayments.map((payment) => (
           <PaymentCard
             key={payment.id}
             onclick={() => dispatch(setUserPayment(payment.id))}
@@ -22,7 +22,14 @@ export default function PaymentSelector ({ children }) {
             provider={payment.provider}
             id={payment.id}
           />
-        ))}
+        ))} */}
+        <PaymentCard
+          onclick={() => dispatch(setUserPayment(3))}
+          paymentType='Pagar con Mercado Pago'
+          expirationDate=''
+          provider=''
+          id={3}
+        />
         <Flex justifyContent='flex-end' mt={5}>
           {children}
         </Flex>
--- client/src/services/payments.js
+++ client/src/services/payments.js
@@ -9,3 +9,66 @@ export const createPayment = function (values) {
     }
   })
 }
+
+// MERCADO PAGO
+export const createMercadoPagoPreferences = async function () {
+  const url = 'https://api.mercadopago.com/checkout/preferences'
+  const { cartProducts, user, order } = store.getState()
+  const orderAddress = user.userAddresses.find(address => address.id === order.userAddressId)
+
+  const addres = {
+    zip_code: orderAddress.postalCode.toString(),
+    street_name: orderAddress.streetName,
+    street_number: orderAddress.houseNumber
+  }
+
+  const items = cartProducts.map(product => ({
+    title: product.name,
+    description: product.description,
+    picture_url: product.image[0],
+    category_id: product.categoryId,
+    quantity: product.quantity,
+    currency_id: 'ARS',
+    unit_price: Number(product.price)
+  }))
+
+  const body = {
+    items,
+    payer: {
+      phone: {},
+      identification: {},
+      addres
+    },
+    payment_methods: {
+      excluded_payment_methods: [
+        {}
+      ],
+      excluded_payment_types: [
+        {}
+      ]
+    },
+    shipments: {
+      free_methods: [
+        {}
+      ],
+      receiver_address: {}
+    },
+    back_urls: {
+      success: 'http://localhost:3000/confirmation',
+      pending: 'http://localhost:3000/confirmation',
+      failure: 'http://localhost:3000/confirmation'
+    },
+    differential_pricing: {},
+    metadata: {
+    }
+  }
+
+  const payment = await axios.post(url, body, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.REACT_APP_MERCADO_PAGO_ACCESS_TOKEN}`
+    }
+  })
+
+  return payment.data
+}
