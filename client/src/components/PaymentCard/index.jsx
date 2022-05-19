67e1125a3946b9b2333103426b662877af226aac
mutriaxx <marcelautria@gmail.com>
2022-05-18 19:34:44 -0500

    fix(client): fixed buttons

--- client/src/components/PaymentCard/index.jsx
+++ client/src/components/PaymentCard/index.jsx
@@ -1,6 +1,7 @@
-import { Box, HStack, VStack } from '@chakra-ui/react'
+import { Box, Flex, VStack, Text, Circle } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 import { MdCheckCircleOutline as CheckIcon } from 'react-icons/md'
+import { FaCreditCard } from 'react-icons/fa'
 
 export default function PaymentCard ({ id, paymentType, cardNumber, expirationDate, provider, onclick }) {
   const cardNumberHidden = `****-****-****-${cardNumber.slice(-4)}`
@@ -8,15 +9,19 @@ export default function PaymentCard ({ id, paymentType, cardNumber, expirationDa
   const imSelected = selectedId === id
 
   return (
-    <Box onClick={onclick}>
-      <HStack>
-        <VStack>
-          <h2>{`${paymentType} ${provider}`}</h2>
-          <p>{`Nro. de Tarjeta: ${cardNumberHidden}`}</p>
-          <p>{`Fecha de vencimiento: ${expirationDate}`}</p>
+    <Flex alignItems='center' bg='white' borderRadius={3} mt={2} mb={2} onClick={onclick}>
+      <Flex w='100%' alignItems='center'>
+        <Box m={4}>
+          <Circle size='40px' bg='secondary' color='white'><FaCreditCard size={20} /></Circle>
+        </Box>
+        <VStack pt={3} pb={3} alignItems='flex-start'>
+          <Text>{`${provider} ${paymentType} ${cardNumberHidden}`}</Text>
+
         </VStack>
-        {imSelected && <CheckIcon />}
-      </HStack>
-    </Box>
+      </Flex>
+      <Flex m={5} justifyContent='center'>
+        {imSelected && <CheckIcon size={20} color='#3182ce' />}
+      </Flex>
+    </Flex>
   )
 }
--- client/src/components/PaymentSelector/index.jsx
+++ client/src/components/PaymentSelector/index.jsx
@@ -2,24 +2,32 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setUserPayment } from '../../redux/actions'
 import PaymentCard from '../PaymentCard'
+import { Text, Flex } from '@chakra-ui/react'
 
-export default function PaymentSelector () {
+export default function PaymentSelector ({ children }) {
   const userPayments = useSelector(state => state.user.userPayments)
   const dispatch = useDispatch()
 
   return (
-    <div>{userPayments &&
-      userPayments.map((payment) => (
-        <PaymentCard
-          key={payment.id}
-          onclick={() => dispatch(setUserPayment(payment.id))}
-          paymentType={payment.paymentType}
-          cardNumber={payment.cardNumber}
-          expirationDate={payment.expirationDate}
-          provider={payment.provider}
-          id={payment.id}
-        />
-      ))}
-    </div>
+    <Flex justifyContent='center' alignItems='center' h='80vh' w='100vw'>
+      <Flex flexDirection='column' borderRadius={5} boxShadow='lg' bg='secondary' w='70vw' p={5}>
+        <Text p={0} fontSize='2xl' fontWeight='500' mb={2}>Métodos de pago </Text>
+        {userPayments && userPayments.map((payment) => (
+          <PaymentCard
+            key={payment.id}
+            onclick={() => dispatch(setUserPayment(payment.id))}
+            paymentType={payment.paymentType}
+            cardNumber={payment.cardNumber}
+            expirationDate={payment.expirationDate}
+            provider={payment.provider}
+            id={payment.id}
+          />
+        ))}
+        <Flex justifyContent='flex-end' mt={5}>
+          {children}
+        </Flex>
+      </Flex>
+
+    </Flex>
   )
 }
