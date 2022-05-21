e20e0f8b2345cb1c6942ebb32db09ed258711c61
Federico Acuna <federicoa1982@gmail.com>
2022-05-21 12:25:17 -0300

    refactor(CLIENT/Addresses redux)

---
+++ client/src/redux/actions/addresses.actions.js
@@ -0,0 +1,92 @@
+import { getAddresses, postAddress, deleteAddress, putAddress } from '../../services/addresses'
+import { GET_ADDRESSES, UPDATE_ADDRESSES } from '../constants'
+
+export const getUserAddresses = () => {
+  return async (dispatch) => {
+    try {
+      const response = await getAddresses()
+
+      dispatch({
+        type: GET_ADDRESSES,
+        payload: response
+      })
+    } catch (error) {
+      dispatch({
+        type: GET_ADDRESSES,
+        payload: error
+      })
+    }
+  }
+}
+
+export const createNewAddress = (newAddress) => {
+  return async (dispatch) => {
+    try {
+      const response = await postAddress(newAddress)
+      response.toast = {
+        title: 'Direccion Agregada.',
+        description: 'Ya puedes recibir ahi tu envío.',
+        status: 'success',
+        duration: 6500,
+        isClosable: true
+      }
+      dispatch({
+        type: UPDATE_ADDRESSES,
+        payload: response
+      })
+    } catch (error) {
+      dispatch({
+        type: UPDATE_ADDRESSES,
+        payload: error
+      })
+    }
+  }
+}
+
+export const removeAddress = (addressId) => {
+  return async (dispatch) => {
+    try {
+      const response = await deleteAddress(addressId)
+      response.toast = {
+        title: 'Direccion Eliminada.',
+        description: 'Ya no recibiras envios ahi.',
+        status: 'success',
+        duration: 6500,
+        isClosable: true
+      }
+      dispatch({
+        type: UPDATE_ADDRESSES,
+        payload: response
+      })
+    } catch (error) {
+      dispatch({
+        type: UPDATE_ADDRESSES,
+        payload: error
+      })
+    }
+  }
+}
+
+export const updateAddress = (addressId) => {
+  return async (dispatch) => {
+    try {
+      const response = await putAddress(addressId)
+      response.toast = {
+        title: 'Direccion Actualizada.',
+        description: 'Tus envios podran llegar ahi.',
+        status: 'success',
+        duration: 6500,
+        isClosable: true
+      }
+      dispatch({
+        type: UPDATE_ADDRESSES,
+        payload: response
+      })
+    } catch (error) {
+      dispatch({
+        type: UPDATE_ADDRESSES,
+        payload: error
+      })
+    }
+  }
+}
