e3d7b0d44ede0db6148172491406bcc0b6d2d2b1
Federico Acuna <51655531+federicoacuna@users.noreply.github.com>
2022-05-22 19:38:46 -0300

    Update system.actions.js

--- client/src/redux/actions/system.actions.js
+++ client/src/redux/actions/system.actions.js
@@ -1,15 +1,14 @@
 import { SET_PAGE} from '../constants' // eslint-disable-line
 
 export function setPageNumber (currentPage) {
-  return dispatch => {
     const payload = {
       value: currentPage,
       name: 'page'
 
     }
-    dispatch({
+    return {
       type: SET_PAGE,
       payload
-    })
+    }
   }
 }
