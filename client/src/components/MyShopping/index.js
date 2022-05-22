1c716f50c7cb8f0f48ef62288ac3ff350cea0140
emanuel <emasaucedo282@gmail.com>
2022-05-22 00:52:49 -0300

    feat(client):styling myShopping component

--- client/src/components/MyShopping/index.js
+++ client/src/components/MyShopping/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import CardOrder from '../CardOrder'
 import { useDispatch, useSelector } from 'react-redux'
 import { getOrdersByUser } from '../../redux/actions'
+import s from './index.module.css'
 
 export default function MyShopping () {
   const dispatch = useDispatch()
@@ -14,7 +15,7 @@ export default function MyShopping () {
   }, [])//eslint-disable-line
 
   return (
-    <div>{orders.length && orders.map(p => (
+    <div className={s.container}>{orders.length && orders.map(p => (
       <CardOrder key={p.id} id={p.id} total={p.total} date={p.createdAt} orderItems={p.products} />
     ))}
     </div>
