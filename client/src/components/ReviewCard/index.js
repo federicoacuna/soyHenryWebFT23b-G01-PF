e71e7eb18889998a3a4db8ccd21685a5319b8d8b
valenbenitez <benitezvalentin046@gmail.com>
2022-05-23 12:01:43 -0300

    fix(client/reviewCard)beatiful card

--- client/src/components/ReviewCard/index.js
+++ client/src/components/ReviewCard/index.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import s from './index.module.css'
 import { AiFillStar } from 'react-icons/ai'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Heading } from '@chakra-ui/react'
 
 export default function ReviewCard ({ rating, review, productId, productName, productImage }) {
-  console.log(rating, review, productId, productName, productImage)
   return (
     <div className={s.container}>
       <div>
-        <p>{productName}</p>
-        <p>{productImage}</p>
+        <Heading as='h4' size='md'>{productName}</Heading>
+        <br />
+        <Heading as='h5' size='sm'>{review}</Heading>
+        <br />
       </div>
-      <div>
-        <p>{review}</p>
-        <Flex alignItems='center' justifyContent='center'>{rating} <AiFillStar className={s.star} /></Flex>
+      <div className={s.div}>
+        <Flex className={s.rat} alignItems='center' justifyContent='center'>{rating} <AiFillStar size={50} className={s.star} /></Flex>
       </div>
+      <div />
       <p>ID:{productId}</p>
 
     </div>
