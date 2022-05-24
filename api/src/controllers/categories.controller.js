3921c4f4e44a197304e3f9fae38fa9de0984e4cd
Luis Nicolás Migliore Sabadini <migliorenicolas@yahoo.com.ar>
2022-05-24 13:32:33 -0300

    Feat(API) added functionalities for categories

--- api/src/controllers/categories.controller.js
+++ api/src/controllers/categories.controller.js
@@ -1,19 +1,54 @@
-const Categories = require('../services/categories.service')
+const CategoriesService = require('../services/categories.service')
 
 const get = async (req, res) => {
   try {
-    const retrievedCategories = await Categories.getAllCategories()
-    retrievedCategories ? res.json(retrievedCategories) : res.status(404).json({ error: 'No categories where found' })
+    const retrievedCategories = await CategoriesService.getAllCategories()
+    retrievedCategories ? res.status(200).json(retrievedCategories) : res.status(404).json({ error: 'No categories where found' })
   } catch (error) {
     res.status(400).json(error)
   }
 }
 
 const create = async (req, res) => {
-  res.json({ message: 'THIS FUNCTION HAS NOT BEEN IMPLEMENTED YET' })
+  try {
+    const category = await CategoriesService.createCategory(req.body)
+    const all = await CategoriesService.getAllCategories()
+    category
+      ? res.status(200).json({
+        message: 'La categoría ha sido agregada exitosamente',
+        payload: all
+      })
+      : res.status(400).json({
+        error: 'La categoría ya se encuentra registrada',
+        payload: all
+      })
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
+
+const remove = async (req, res) => {
+  const { categoryId } = req.params
+
+  try {
+    const wasDeleted = await CategoriesService.removeCategory(categoryId)
+    const all = await CategoriesService.getAllCategories()
+    wasDeleted
+      ? res.status(200).json({
+        message: 'La categoría ha sido eliminada exitosamente',
+        payload: all
+      })
+      : res.status(400).json({
+        error: 'La categoría no se pudo eliminar',
+        payload: all
+      })
+  } catch (error) {
+    res.status(400).json(error)
+  }
 }
 
 module.exports = {
   get,
-  create
+  create,
+  remove
 }
