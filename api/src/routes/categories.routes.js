9d2beacd825e126cd58b089e319b4633b6dfc88b
Luis Nicolás Migliore Sabadini <migliorenicolas@yahoo.com.ar>
2022-05-24 11:56:43 -0300

    Update categories.routes.js
    Se modificó la ubicación del middleware para que no rompa al cargar el Home principal

--- api/src/routes/categories.routes.js
+++ api/src/routes/categories.routes.js
@@ -3,9 +3,9 @@ const router = Router()
 const { get, create, remove } = require('../controllers/categories.controller')
 const middleware = require('../middleware')
 
-router.use(middleware.decodeToken)
-
 router.get('/', get)
+
+router.use(middleware.decodeToken)
 router.post('/', create)
 router.delete('/:categoryId', remove)
 
