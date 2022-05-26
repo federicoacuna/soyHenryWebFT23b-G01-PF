18fa05c1cb99b6b2932ac98bb7d07ba85004ebbb
Federico Acuna <federicoa1982@gmail.com>
2022-05-26 15:56:31 -0300

    fix(CLIENT/actions)

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -13,9 +13,23 @@ export const sendToken = async (token) => {
   return data
 }
 
+export const getUsers = async () => {
+  const { token } = store.getState().users
+  const { data } = await axios.get('/', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  return data
+}
+
 export const updateUser = async function (newData) {
   const { token } = store.getState().users
-  const { data } = await axios.put(endpoint, newData, {
+  let urlString = endpoint
+  if (newData.userId) urlString += `/${newData.userId}`
+
+  const { data } = await axios.put(urlString, newData, {
     headers: {
       Authorization: `Bearer ${token}`
     }
