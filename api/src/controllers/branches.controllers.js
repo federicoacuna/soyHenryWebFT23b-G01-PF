a0f92d8e594df8848770b74c65855a123c6ca3d0
Jyankc <juanyancovich@gmail.com>
2022-05-25 21:59:58 -0300

    changes in back end responses

--- api/src/controllers/branches.controllers.js
+++ api/src/controllers/branches.controllers.js
@@ -3,7 +3,7 @@ const branchService = require('../services/branches.service')
 const get = async (req, res) => {
   try {
     const branches = await branchService.getBranches()
-    res.status(200).json({ data: branches, message: 'Sucursales obtenidas con exito' })
+    res.status(200).json(branches)
   } catch (error) {
     res.status(400).json(error)
   }
@@ -13,8 +13,9 @@ const create = async (req, res) => {
   try {
     if (!state || !city || !streetName || !houseNumber || !countryId) { return res.status(401).json({ message: 'Faltan datos para poder agregar sucursal' }) } else if (phoneNumber && ((isNaN((parseInt(phoneNumber)))))) { return res.status(401).json({ mesagge: 'El telefóno solo puede contener numeros' }) } else {
       const newBranch = await branchService.addNewBranch(req.body)
-      if (Array.isArray(newBranch)) { return res.status(400).json({ message: 'La sucursal ya se encuentra agregada.  ' }) } else {
-        res.status(200).json({ data: newBranch, message: 'sucursal agregada con exito' })
+      if (Array.isArray(newBranch)) { return res.status(400).json({ message: 'La sucursal ya se encuentra agregada' }) } else {
+        const branches = await branchService.getBranches()
+        res.status(200).json({ data: branches, message: 'sucursal agregada con exito' })
       }
     }
   } catch (error) {
@@ -29,7 +30,8 @@ const update = async (req, res) => {
     if (!id || !state || !city || !streetName || !houseNumber || !countryId) { return res.status(400).json({ message: 'Faltan datos para poder modificar la sucursal' }) } else if (phoneNumber && ((isNaN((parseInt(phoneNumber)))))) { return res.status(401).json({ mesagge: 'El telefóno solo puede contener numeros' }) } else {
       const modifiedBranch = await branchService.modifyBranch(req.body)
       if (!modifiedBranch) { return res.status(404).json({ message: 'La sucursal que busca modificar no existe o fue eliminada' }) } else {
-        res.status(200).json({ data: modifiedBranch, message: 'Sucursal modificada con exito' })
+        const branches = await branchService.getBranches()
+        res.status(200).json({ data: branches, message: 'Sucursal modificada con exito' })
       }
     }
   } catch (error) {
@@ -44,7 +46,8 @@ const branchStatus = async (req, res) => {
     if (!(typeof parseInt(id) === 'number')) { return res.status(400).json({ message: 'Por favor ingrese un ID numerico' }) }
     const deletedBranch = await branchService.changeBranchStatus(id)
     if (!deletedBranch) { return res.status(404).json({ message: 'La sucursal que busca eliminar no existe' }) }
-    res.status(200).json({ data: deletedBranch, message: 'Sucursal eliminada con exito' })
+    const branches = await branchService.getBranches()
+    res.status(200).json({ data: branches, message: 'Sucursal eliminada con exito' })
   } catch (error) {
     res.status(400).json(error)
   }
