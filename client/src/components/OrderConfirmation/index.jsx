4a6ee3eb1f41534e131299b20eaf625ac185f14c
Federico Acuna <51655531+federicoacuna@users.noreply.github.com>
2022-05-25 22:06:53 -0300

    Update index.jsx

--- client/src/components/OrderConfirmation/index.jsx
+++ client/src/components/OrderConfirmation/index.jsx
@@ -7,8 +7,6 @@ import AddressCard from '../AddressCard'
 import UserPaymentCard from '../PaymentCard'
 import { getOrderDetails } from '../../redux/actions/orders.actions'
 import { BsFillCheckCircleFill } from 'react-icons/bs'
-// import s from './index.module.css'
-// import LoadingSpinner from '../LoadingSpinner'
 
 export default function OrderConfirmation () {
   const dispatch = useDispatch()
@@ -92,59 +90,6 @@ export default function OrderConfirmation () {
         </div>}
         </Box>
       </Flex>
-
     </Flex>
-    // <div className={s.containerAll}>
-    //   <div className={s.container}>
-    //     <Box bg='success' p='1rem' borderRadius='0.8rem'>
-    //       {createdOrder?.id
-    //         ? <div>
-    //           <p>Nro. de Orden: {createdOrder.id}</p>
-    //           <p>Fue pagado con:</p>
-    //           <UserPaymentCard
-    //             key={userPayment.id}
-    //             paymentType={userPayment.paymentType}
-    //             cardNumber={userPayment.cardNumber}
-    //             expirationDate={userPayment.expirationDate}
-    //             provider={userPayment.provider}
-    //             id={userPayment.id}
-    //           />
-    //           {userAddress && <p>Su orden sera despachada a:</p>}
-    //           {
-    //       userAddress &&
-    //         <AddressCard
-    //           id={userAddress.id}
-    //           postalCode={userAddress.postalCode}
-    //           streetName={userAddress.streetName}
-    //           houseNumber={userAddress.houseNumber}
-    //           city={userAddress.city}
-    //           state={userAddress.state}
-    //           country={userAddress.country}
-    //         />
-    //       }
-    //           {branch && <p>Su orden estara disponible para su retiro en:</p>}
-    //           {
-    //       branch &&
-    //         <AddressCard
-    //           id={branch.id}
-    //           streetName={branch.streetName}
-    //           houseNumber={branch.houseNumber}
-    //           city={branch.city}
-    //           state={branch.state}
-    //           country={branch.country}
-    //         />
-    //       }
-    //           <UnorderedList>
-    //             {createdOrder.orderItems.map((item, index) => <ListItem key={index}>{item.name}</ListItem>)}
-    //           </UnorderedList>
-    //           <Button border='1px' borderColor='white' _hover={{ color: 'white' }} bg='success' color='white' onClick={handleClick}>Aceptar</Button>
-    //           {/* eslint-disable-next-line */}
-    //     </div>
-    //         : <p>A</p>}
-    //     </Box>
-    //   </div>
-
-  // </div>
-
   )
 }
